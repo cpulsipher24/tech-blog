44f34c26bb5137bf35f994f0ef7aa1ac8cbb99e6
Cpulsipher24 <collintpulsipher@gmail.com>
2024-02-04 22:38:02 -0700

    Set configuration for db

---
+++ config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    development: {
+      username: 'cpulsipher',
+      password: 'Collinp24',
+      database: 'tech_blog',
+      host: 'localhost',
+      dialect: 'mysql',
+    },
+    test: {
+      username: 'cpulsipher',
+      password: 'Collinp24',
+      database: 'tech_blog',
+      host: 'localhost',
+      dialect: 'mysql',
+    },
+    production: {
+      username: 'cpulsipher',
+      password: 'Collinp24',
+      database: 'tech_blog',
+      host: 'localhost',
+      dialect: 'mysql',
+    },
+  };
+  
