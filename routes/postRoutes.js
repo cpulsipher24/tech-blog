7f79fb738bb890aba61ac35a7cb2723edfaae026
Cpulsipher24 <collintpulsipher@gmail.com>
2024-02-06 23:04:44 -0700

    Updated postRoutes.js file

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,12 +3,22 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
 
-// Routes for blog functionality
+// Route to get all blog posts
 router.get('/posts', postController.getAllPosts);
+
+// Route to display individual blog post
 router.get('/posts/:id', postController.getPostById);
+
+// Route to create a new blog post
 router.post('/posts', postController.createPost);
+
+// Route to update an existing blog post
 router.put('/posts/:id', postController.updatePost);
+
+// Route to delete a blog post
 router.delete('/posts/:id', postController.deletePost);
+
+// Route to create a new comment on a blog post
 router.post('/posts/:id/comments', postController.createComment);
 
 module.exports = router;
