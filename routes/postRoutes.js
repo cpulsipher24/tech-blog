6928033f5a80372e6bcbb144a65573f1024bcc42
Cpulsipher24 <collintpulsipher@gmail.com>
2024-02-09 20:02:59 -0700

    Added proper routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,27 +2,27 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
-const requireAuth = require('../middleware/authMiddleware').requireAuth;
+const { requireAuth } = require('../middleware/authMiddleware');
 
 // Route to display the homepage with existing blog posts
 router.get('/', postController.getHomepage);
 
 // Route to get all blog posts
-router.get('/', postController.getAllPosts);
+router.get('/posts', postController.getAllPosts);
 
 // Route to display individual blog post
-router.get('/:id', postController.getPostWithComments);
+router.get('/posts/:id', postController.getPostWithComments);
 
 // Route to create a new blog post
-router.post('/', requireAuth, postController.createPost);
+router.post('/posts', requireAuth, postController.createPost);
 
 // Route to update an existing blog post
-router.put('/:id', requireAuth, postController.updatePost);
+router.put('/posts/:id', requireAuth, postController.updatePost);
 
 // Route to delete a blog post
-router.delete('/:id', requireAuth, postController.deletePost);
+router.delete('/posts/:id', requireAuth, postController.deletePost);
 
 // Route to create a new comment on a blog post
-router.post('/:id/comments', requireAuth, postController.createComment);
+router.post('/posts/:id/comments', requireAuth, postController.createComment);
 
 module.exports = router;
